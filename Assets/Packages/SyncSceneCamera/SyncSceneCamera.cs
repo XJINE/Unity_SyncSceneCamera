12b26e7f227bf2f68478f15842b4628e5097bc59
XJINE <xjine.andorelseif@gmail.com>
2019-05-21 16:05:30 +0900

    First commit.

---
+++ Assets/Packages/SyncSceneCamera/SyncSceneCamera.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[ExecuteAlways]
+public class SyncSceneCamera : MonoBehaviour
+{
+    #region Field
+
+    public Camera target;
+
+    #endregion Field
+
+    #region Method
+
+    protected virtual void OnEnable()
+    {
+        SceneView.duringSceneGui += SyncCameraTransform;
+    }
+
+    protected virtual void OnDisable()
+    {
+        SceneView.duringSceneGui -= SyncCameraTransform;
+    }
+
+    protected virtual void SyncCameraTransform(SceneView sceneView)
+    {
+        Handles.BeginGUI();
+
+        if (GUILayout.Button("Sync Camera", GUILayout.Width(100)))
+        {
+            Camera camera = this.target ?? Camera.main;
+
+            if (camera)
+            {
+                sceneView.AlignViewToObject(camera.transform);
+            }
+        }
+
+        Handles.EndGUI();
+    }
+
+    #endregion Method
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Unity_SyncSceneCamera
+
 Sync scene camera to game camera.
+
+<img src="https://github.com/XJINE/Unity_SyncSceneCamera/blob/master/Screenshot.png" width="100%" height="auto" />
